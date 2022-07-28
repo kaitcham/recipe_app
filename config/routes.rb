b91d21e8b98f4951622116b7a7e6842b3e9c43c8
Munezero Christian <chamsf5@aol.com>
2022-07-28 18:35:07 +0200

    Allow users to see public recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :foods
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   resources :recipes
+  get '/public_recipes', to: 'public_recipes#index'
   # Defines the root path route ("/")
   root "home#index"
 end
