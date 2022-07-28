e26bffb0cead3c8933030daf9633da47e32ba1a4
Munezero Christian <chamsf5@aol.com>
2022-07-28 23:56:18 +0200

    modified files based on wireframes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.all.where(public: true).order(id: :desc)
+    @public_recipes = Recipe.includes(:recipe_foods, :user).where('public = true').order(id: :desc)
   end
 end
